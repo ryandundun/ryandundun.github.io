46f3e96606dfa68052966e5bfb54f144b75b8388
Ryan Dundun <dundun@wisc.edu>
2019-01-27 14:48:47 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+
+/* ---- Typed.js config ---- */
+var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 40,
+  backSpeed: 25,
+  backDelay: 900,
+  loop: true,
+});
